b6833b6b792b108e59ecb253b4f85bde42fccc7a
alk3pInjection <webmaster@raspii.tech>
2020-05-26 10:37:52 +0800

    IceKernel: Initialize
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>

--- build_master.sh
+++ build_master.sh
@@ -1,44 +1,35 @@
 #!/bin/bash
 
+VERSION="$(cat version)"
+
 if [[ "${1}" != "skip" ]] ; then
 	./build_clean.sh
-	./build_kernel.sh stock "$@" || exit 1
 fi
 
-VERSION="$(cat version)-$(date +%F | sed s@-@@g)"
+./build_kernel.sh || exit 1
 
-if [ -e boot.img ] ; then
-	rm arter97-kernel-$VERSION.zip 2>/dev/null
-	cp boot.img arter97-kernel-$VERSION.img
+if [ -e arch/arm64/boot/Image.gz ] ; then
+	echo
+	echo "Building Kernel Package"
+	echo
 
-	# Pack AnyKernel3
-	rm -rf kernelzip
+	rm IceKernel-$VERSION.zip 2>/dev/null
+	rm -rf kernelzip 2>/dev/null
+
 	mkdir kernelzip
-	echo "
-kernel.string=arter97 kernel $(cat version) @ xda-developers
-do.devicecheck=1
-do.modules=0
-do.cleanup=1
-do.cleanuponabort=0
-device.name1=OnePlus7
-device.name2=OnePlus7Pro
-device.name3=OnePlus7T
-device.name4=OnePlus7TPro
-device.name5=guacamoleb
-device.name6=guacamole
-device.name7=hotdogb
-device.name8=hotdog
-block=/dev/block/bootdevice/by-name/boot
-is_slot_device=1
-ramdisk_compression=gzip
-" > kernelzip/props
-	cp -rp ~/android/anykernel/* kernelzip/
-	find arch/arm64/boot/dts -name '*.dtb' -exec cat {} + > kernelzip/dtb
+	cp -rp ./anykernel/* kernelzip/
+	if ( echo $(git branch | sed -n '/\* /s///p') | grep -q 'stock' ); then
+		find arch/arm64/boot/dts -name '*.dtb' -exec cat {} + > kernelzip/dtb
+	fi
 	cd kernelzip/
-	7z a -mx9 arter97-kernel-$VERSION-tmp.zip *
-	7z a -mx0 arter97-kernel-$VERSION-tmp.zip ../arch/arm64/boot/Image.gz
-	zipalign -v 4 arter97-kernel-$VERSION-tmp.zip ../arter97-kernel-$VERSION.zip
-	rm arter97-kernel-$VERSION-tmp.zip
+	7z a -mx9 IceKernel-$VERSION-tmp.zip *
+	if ( echo $(git branch | sed -n '/\* /s///p') | grep -q 'stock' ); then
+		7z a -mx0 IceKernel-$VERSION-tmp.zip ../arch/arm64/boot/Image.gz
+	else
+		7z a -mx0 IceKernel-$VERSION-tmp.zip ../arch/arm64/boot/Image.gz-dtb
+	fi
+	zipalign -v 4 IceKernel-$VERSION-tmp.zip ../IceKernel-$VERSION.zip
+	rm IceKernel-$VERSION-tmp.zip
 	cd ..
-	ls -al arter97-kernel-$VERSION.zip
+	ls -al IceKernel-$VERSION.zip
 fi
