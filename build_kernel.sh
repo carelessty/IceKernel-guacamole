95b832246bb32148634bcd8a8e1721bbab8038a2
carelessty <294893644@protonmail.com>
2020-07-11 19:12:44 +0800

    build: Set Kbuild info properly
    Signed-off-by: carelessty <294893644@protonmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -8,6 +8,8 @@ export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=../build-tools/proton-clang/bin/aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=../build-tools/proton-clang/bin/arm-linux-gnueabi-
 export LD_LIBRARY_PATH=../build-tools/proton-clang/lib:$LD_LIBRARY_PATH
+export KBUILD_BUILD_USER=OnePlus
+export KBUILD_BUILD_HOST=rd-build-73
 
 echo
 echo "Setting defconfig"
